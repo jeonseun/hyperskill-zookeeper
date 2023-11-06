00586e246d50c5316b71ce52be5be4f7960ed832
jeonseun <jeonseun1234@gmail.com>
2023-11-06 22:25:04 +0900

    chore: hyperskill 학습 관련 내용 제거

--- Topics/Primitive and reference types/Creating a primitive/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // put your code here
-        int java1ReleaseYear = 1996;
-        System.out.println(java1ReleaseYear);
-    }
-}
--- Topics/String/Statements/src/Task.java
+++
@@ -1,9 +0,0 @@
-// You can experiment here, it won’t be checked
-
-public class Task {
-  public static void main(String[] args) {
-    // put your code here
-    char ch = '5';
-    System.out.printf("%c", ch);
-  }
-}
